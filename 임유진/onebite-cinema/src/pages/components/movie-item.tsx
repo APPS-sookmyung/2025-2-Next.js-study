ae932f7859717a4c5421c288685ee3006196fc63
임유진 <lyj5721@sookmyung.ac.kr>
2025-09-26 04:06:38 +0900

    feat: 3주차 실습 코드 및 과제 제출

---
+++ 임유진/onebite-cinema/src/pages/components/movie-item.tsx
@@ -0,0 +1,20 @@
+import type { MovieData } from "@/types";
+import Link from "next/link";
+
+export default function MovieItem({
+  id,
+  title,
+  subTitle,
+  description,
+  releaseDate,
+  company,
+  genres,
+  runtime,
+  posterImgUrl,
+}: MovieData) {
+  return (
+    <Link href={`/movie/${id}`}>
+      <img src={posterImgUrl} />
+    </Link>
+  );
+}
--- 임유진/onebite-cinema/src/pages/search/index.tsx
+++ 임유진/onebite-cinema/src/pages/search/index.tsx
@@ -1,11 +1,35 @@
 import { useRouter } from "next/router";
 import SearchableLayout from "../components/searchable-layout";
 import { ReactNode } from "react";
+import movie from "@/mock/dummy.json";
+import MovieItem from "@/pages/components/movie-item";
+import style from "./index.module.css";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchMovies from "@/lib/fetch-movies";
 
-export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-  return <h1>검색 결과: {q}</h1>;
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
+
+  return {
+    props: {
+      movies,
+    },
+  };
+};
+
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className={style.container}>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
