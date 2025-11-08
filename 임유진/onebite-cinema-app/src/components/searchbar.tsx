b0719d7d3d7f79acda7160ec6ca344e0a81f3bce
임유진 <lyj5721@sookmyung.ac.kr>
2025-11-09 03:10:10 +0900

    feat: 6주차 미션 제출

--- 임유진/onebite-cinema-app/src/components/searchbar.tsx
+++ 임유진/onebite-cinema-app/src/components/searchbar.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import style from "./searchbar.module.css";
 
 export default function Searchbar() {
   const router = useRouter();
   const searchParams = useSearchParams();
-
-  const q = searchParams.get("q") as string;
-
   const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
 
   useEffect(() => {
     setSearch(q || "");
   }, [q]);
 
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   const onSubmit = () => {
     if (!search || q === search) return;
     router.push(`/search?q=${search}`);
-  };
-
-  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -31,14 +31,14 @@ export default function Searchbar() {
   };
 
   return (
-    <div>
+    <div className={style.container}>
       <input
         value={search}
         onChange={onChangeSearch}
         onKeyDown={onKeyDown}
-        placeholder="검색어를 입력하세요..."
+        placeholder="검색어를 입력하세요 ..."
       />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
