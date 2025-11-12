740e14f746e9ff0cf74ed62756d8952263ff2da8
peisonger <realsonge@sookmyung.ac.kr>
2025-11-12 19:26:48 +0900

    Feat: 6주차 과제 및 강의정리

---
+++ week6/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,28 @@
+import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
+import style from "./page.module.css";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  const response = await fetch(`http://localhost:12345/movie/search?q=${q}`, {
+    cache: "force-cache",
+  });
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.container}>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
